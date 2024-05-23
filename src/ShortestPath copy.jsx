dd0cbd18314905abf1c440dcecda0e156b6be796
Robert Antonius <robert.antonius23@gmail.com>
2024-05-23 21:39:24 +0700

    Finished prototype

--- src/ShortestPath copy.jsx
+++ src/ShortestPath copy.jsx
@@ -347,253 +347,77 @@ function App() {
         ]
       })
     const [heightMap, setHeightMap] = useState([])
-    const [start, setStart] = useState([0, 0])
-    const [end, setEnd] = useState([0, 0])
-    const [path, setPath] = useState([])
-    const [visited, setVisited] = useState([])
-    const [options, setOptions] = useState([])
 
-    const [threshold, setThreshold] = useState(3)
-    const [speed, setSpeed] = useState(10)
-    const [recencyWeight, setRecencyWeight] = useState(1)
-    const [elevationWeight, setElevationWeight] = useState(2)
-
-    const [trigger, setTrigger] = useState(0)
-    const [step, setStep] = useState(0)
-    const [timer, setTimer] = useState("")
-    const [aspectRatio, setAspectRatio] = useState("")
-
-    // ran once
-    const charToInt = (c, rowIndex, colIndex) => {
-        if (c == "S") setStart((e) => [rowIndex, colIndex])
-        if (c == "E") setEnd((e) => [rowIndex, colIndex])
+    const charToInt = (c) => {
         let h = c.charCodeAt(0)
-        return (h >= 97 && h <= 122 ? h - 96 : (c == "E" ? 27 : (c == "S" ? 0 : 29)))
+        return ((h >= 65 && h <= 90) ? (h - 64 + 26) : ((h >= 97 && h <= 122) ? (h - 96) : 0))
     }
 
-    // ran once
     const toHeightMap = (s) => {
-        return s.split("\n").map((e, rowIndex) => e.split("").filter((f) => f != "\r").map((g, colIndex) => charToInt(g, rowIndex, colIndex)))
+        return s.split("\n").map((e) => e.split("").filter((f) => f != "\r").map((g) => charToInt(g)))
     }
 
-    // ran for every cell
-    const getColor = (e, rowIndex, colIndex) => {
-        // hue is static
-        let saturation = 50 - (e * 2)
-        let light = Math.round(e / 27 * 60) + 10
-        
-        // start
-        if(heightMap[rowIndex][colIndex] == 0) {
-            return "blue"
-        }
-        // end
-        if(heightMap[rowIndex][colIndex] == 27) {
-            return "red"
-        }
-        // tiles that the algorithm considered
-        if(options && options.length && options[rowIndex][colIndex]) {
-            return "hsl(" + (10) + ", " + (80) + "%, " + (light) + "%)"
-        }
-        // tiles that the algorithm checked
-        if(visited && visited.length && visited[rowIndex][colIndex]) {
-            return "hsl(" + (100) + ", " + (80) + "%, " + (light) + "%)"
-        }
-        // tiles that the algorithm chose
-        if(path && path.length && path[rowIndex][colIndex]) {
-            return "hsl(" + (220) + ", " + (80) + "%, " + (light+10) + "%)"
-        }
-        return "hsl(" + (80) + ", " + (saturation) + "%, " + (light) + "%)"
-    }
-
-    // ran every file upload
     const handleFileChange = async (e) => {
-        resetRender()
-        setHeightMap((e) => [])
         e.preventDefault()
         const reader = new FileReader()
         reader.onload = async (e) => { 
             const text = (e.target.result)
-            setHeightMap((e) => toHeightMap(text))
+            console.log(toHeightMap(text))
         };
         reader.readAsText(e.target.files[0])
     }
 
-    // ran every frame (blegh) (bottleneck)
-    const solve = () => {
-        // it's more efficient to start from the end to avoid trap craters
-        // starting from the end ensures that you only go to place you could
-        // exit from rather than places you could enter in
-        const startingPoint = [...end, 0, 0, 0, 0]
-        const queue = [startingPoint]
-        const options = heightMap.map((e) => e.map((f) => false))
-        const visited = heightMap.map((e) => e.map((f) => false))
-        const paths = []
-        let count = 0
-
-        while (queue.length && count < step) {
-            queue.sort((a, b) => (a[2] - b[2]) * elevationWeight + (a[5] - b[5]) * recencyWeight)
-            const vertex = queue.shift();
-
-            if (!visited[vertex[0]][vertex[1]]) {
-                const x = vertex[1]
-                const y = vertex[0]
-                count = count + 1
-
-                visited[y][x] = true
-                options[y][x] = false
-                paths.push([[vertex[3], vertex[4]], [y, x]])
-
-                for (let dir of [1, -1]) {
-                    for (let move of [[dir, 0], [0, dir]]) {
-                        let x2 = x + move[0]
-                        if (x2 >= 0 && x2 < heightMap[y].length && !visited[y][x2] && !options[y][x2] && traversable(x, y, x2, y)) {
-                            queue.push([y, x2, heightMap[y][x2], y, x, vertex[5] + 1])
-                            options[y][x2] = true
-                        }
-                        let y2 = y + move[1]
-                        if (y2 >= 0 && y2 < heightMap.length && !visited[y2][x] && !options[y2][x] && traversable(x, y, x, y2)) {
-                            queue.push([y2, x, heightMap[y2][x], y, x, vertex[5] + 1])
-                            options[y2][x] = true
-                        }
-                    }
-                }
-            }
-        }
-
-
-        setOptions((e) => options)
-        setVisited((e) => visited)
-
-        if(visited[start[0]][start[1]]) {
-            pauseRender()
-            const optimalPath = heightMap.map((e) => e.map((f) => false))
-            var currentCell = [start[0], start[1]]
-            optimalPath[start[0]][start[1]] = true
-            while (currentCell[0] != end[0] || currentCell[1] != end[1]) {
-                let bestPath = paths.filter((e) => e[1][0] == currentCell[0] && e[1][1] == currentCell[1])
-                currentCell = [bestPath[0][0][0], bestPath[0][0][1]]
-                optimalPath[currentCell[0]][currentCell[1]] = true
-            }
-            setOptions((e) => [])
-            setVisited((e) => [])
-            setPath((e) => optimalPath)
-        }
-    }
-
-    const traversable = (x1, y1, x2, y2) => {
-        return heightMap[y2][x2] - heightMap[y1][x1] >= -threshold 
-    }
-
-    const pauseRender = (offset = 0) => {
-        clearInterval(timer)
-        setTimer((e) => "")
-        if (offset === "reset") {
-            resetRender()
-            return
-        }
-        setStep((e) => e + offset);
-    }
-
-    const resetRender = () => {
-        setPath((e) => [])
-        setVisited((e) => [])
-        setOptions((e) => [])
-        setStep((e) => 0)
-    }
-
-    const getClassName = () => {
-        let aspects = aspectRatio.split(" / ")
-        return "heightmap " + ((aspects[0] / aspects[1]) > (4/3) ? "heightmap-bandaid" : "")
-    }
-
-    useEffect(() => {
-        console.log(heightMap)
-        if (heightMap && heightMap.length) {
-            setAspectRatio((e) => heightMap[0].length + " / " + heightMap.length)
-            pauseRender("reset")
-        }
-    }, [heightMap])
-
-    useEffect(() => {
-        if (heightMap && heightMap.length) solve()
-    }, [step, trigger])
-
-    useEffect(() => {
-        return () => clearInterval(timer)
-    }, []);
-
     return <div className="panel-view" style={{backgroundImage: `url(${backgroundImage})`}}>
         <div className="left-panel">
             <div className="data-table">
-                <form>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th colSpan={2}>Height Map Traversal</th>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nama Kota</th>
+                            <th colSpan={2}>Posisi Kota</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input id="file" type="file" onChange={handleFileChange} />
+                            </td>
+                        </tr>
+                        {data.nodes.map((node) => (
+                                <tr className="text-center">
+                                <td className="text-start">{node.id}</td>
+                                <td>231</td>
+                                <td>-2913</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td colSpan={2}>
-                                    <input id="file" type="file" onChange={handleFileChange} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Threshold</td>
-                                <td><input id="threshold" type="number" value={threshold} onChange={(e) => setThreshold((f) => e.target.value)}/></td>
-                            </tr>
-                            <tr>
-                                <td>Speed</td>
-                                <td><input id="speed" type="number" value={speed} onChange={(e) => setSpeed((f) => e.target.value)}/></td>
-                            </tr>
-                            <tr>
-                                <td>Recency Weight</td>
-                                <td><input id="recency-weight" type="number" value={recencyWeight} onChange={(e) => setRecencyWeight((f) => e.target.value)}/></td>
-                            </tr>
-                            <tr>
-                                <td>Elevation Weight</td>
-                                <td><input id="elevation-weight" type="number" value={elevationWeight} onChange={(e) => setElevationWeight((f) => e.target.value)}/></td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                </form>
+                        ))}
+                        
+                    </tbody>
+                </table>
             </div>
             <div className="control-panel">
-                <button className="control-button control-button-pause" onClick={() => {pauseRender(0)}}>
+                <button className="control-button control-button-pause">
                     <img src={pauseIcon} alt=""></img>
                 </button>
-                <button className="control-button control-button-prev" onClick={() => {pauseRender(-1)}}>
+                <button className="control-button control-button-prev">
                     <img src={prevIcon} alt=""></img>
                 </button>
-                <button className="control-button control-button-next" onClick={() => {pauseRender(1)}}>
+                <button className="control-button control-button-next">
                     <img src={nextIcon} alt=""></img>
                 </button>
-                <button className="control-button control-button-watch" onClick={() => {pauseRender(700)}}>
+                <button className="control-button control-button-watch">
                     <img src={watchIcon} alt=""></img>
                 </button>
-                <button className="control-button control-button-run" onClick={() => {
-                    setTimer((e) => setInterval(() => {
-                        setStep((e) => e + 1)
-                        setTrigger((e) => e + 1)
-                    }, speed))
-                }}>
+                <button className="control-button control-button-run">
                     <img src={runIcon} alt=""></img>
                 </button>
             </div>
         </div>
         <div className="right-panel">
-            {heightMap.length ? <div className={getClassName()} style={{aspectRatio: aspectRatio}}>
-                {heightMap.map((row, rowIndex) => (
-                    <div key={rowIndex} className="heightmap-row">
-                        {row.map((col, colIndex) => (
-                            <div key={colIndex} className={"heightmap-cell "} style={{backgroundColor: getColor(col, rowIndex, colIndex)}}>
-                                
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </div> : <div className="m-auto bg-slate-100 p-4 rounded-md">Please load your height map</div>} 
+            <ForceGraph2D
+                width={window.innerWidth}
+                height={window.innerHeight}
+                graphData={data}
+            />
         </div>
     </div>
 } 
